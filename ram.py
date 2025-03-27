e3cd6d67cedcac5810df6296e7f04978ada0f10e
Krishna27Singh <krishnasingh97857@gmail.com>
2025-03-27 13:40:40 +0530

    ram

---
+++ ram.py
@@ -0,0 +1,29 @@
+# first code
+# for i in range(1, 6):
+#     print(*range(1, i+1))
+
+# sec
+# for i in range(1, 6):
+#     print(" "*(5-i) + "* " * i)
+
+# thi
+# for i in range(5, 0, -1):
+#     print("* " * i)
+
+# four
+# n = 5
+# for i in range(1, n + 1):
+#     print(" " * (n - i), end="")
+#     for j in range(1, i + 1):
+#         print(j, end=" ")
+#     print()
+# print("1 2 3 4 5 6 7 8 9")                   
+
+# five
+# for i in range(9, 0, -2):
+#     print(" " * ((9 - i) // 2), *range(1, i + 1))
+# for i in range(3, 10, 2):
+#     print(" " * ((9 - i) // 2), *range(1, i + 1))
+
+
+print(*range(1,5))
