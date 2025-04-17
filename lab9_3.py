7b46111ab67ee7a1388a56bcd6012a13b14d80da
Krishna27Singh <krishnasingh97857@gmail.com>
2025-04-17 14:44:36 +0530

    lol

--- lab9_3.py
+++ lab9_3.py
@@ -1,4 +1,9 @@
-strings = list(input("Enter the strings (comma separated): ").split(','))
-new_list = [i for i in strings if i[0]=='a'or i[0]=='e'or i[0]=='i'or i[0]=='o' or i[0]=='u']
-print(strings)
-print(new_list)
+# strings = list(input("Enter the strings (comma separated): ").split(','))
+# new_list = [i for i in strings if i[0]=='a'or i[0]=='e'or i[0]=='i'or i[0]=='o' or i[0]=='u']
+# print(strings)
+# print(new_list)
+
+
+data = [("personX", 10), ("personY", 11),("personZ", 31)]
+answer = [i for i in data if i[1]>30]
+print(answer)
